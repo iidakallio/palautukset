323b661d707f9b466e52261793a547fa73d0a1ca
ikallio <iida.kallio@aalto.fi>
2024-11-20 16:49:50 +0200

    1.6 toteutus ja erittely tehtävästä 1.5

---
+++ osa1/1.1-1.5/src/main.jsx
@@ -0,0 +1,17 @@
+import ReactDOM from 'react-dom/client'
+
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+//test
