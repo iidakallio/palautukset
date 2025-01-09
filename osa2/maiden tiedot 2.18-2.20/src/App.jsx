10efcee23bb463efd900a82a7ea065cc73212c2b
ikallio <iida.kallio@aalto.fi>
2025-01-09 18:12:41 +0200

    2.18

--- osa2/maiden tiedot 2.18-2.20/src/App.jsx
+++ osa2/maiden tiedot 2.18-2.20/src/App.jsx
@@ -4,11 +4,44 @@ import axios from 'axios'
 import Filter from './components/Filter'
 
 const CountryList = ({countries, onSelect}) => {
-  //näytetään maiden nimet jos alle 10
+  if (countries.length > 10) {
+    return <p>Too many matches, specify another filter.</p>
+  }
+  else if (countries.length > 1) {
+    return(
+      <ul>
+      {countries.map((country) => (
+        <li key={country.name.common}>
+          {country.name.common}
+        </li>
+      ))}
+    </ul>
+    )
+  }
+  else{
+    return null
+  }
 }
 
-const CounrtyInformation = ({country}) => {
-  //Näytetään selected maan tiedot
+const CountryInformation = ({country}) => {
+  return (
+    <div>
+      <h2>{country.name.common}</h2>
+      <p>Capital: {country.capital}</p>
+      <p>Area: {country.area}</p>
+      <h3>Languages: </h3>
+      <ul>
+        {Object.values(country.languages).map((language) => (
+          <li key={language}>{language}</li>
+        ))}
+      </ul>
+      <img 
+        src={country.flags.png}
+        alt={`Flag of ${country.name.common}`}
+        style={{width: "200px"}}
+      />
+    </div>
+  )
 }
 
 
@@ -21,9 +54,38 @@ const App = () => {
     setFilter(event.target.value)
   }
 
+  const handleSelectCountry = (countryName) => {
+    const country = countries.find((country) => country.name.common === countryName)
+    setSelectedCountry(country)
+  }
+
+useEffect(() => {
+  if (filter) {
+    axios
+    .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
+    .then((response) => {
+      const filtered = response.data.filter((country) =>
+        country.name.common.toLowerCase().includes(filter.toLowerCase())
+      )
+      setCountries(filtered)
+      if (filtered.length === 1) {
+        setSelectedCountry(filtered[0])
+      } else {
+        setSelectedCountry(null)
+      }
+    })
+  }
+  else {
+    setCountries([])
+    setSelectedCountry(null)
+  }
+}, [filter])
+
   return (
     <div>
     <Filter filter={filter} handleFilterChange={handleFilterChange}/>
+    {selectedCountry ? 
+    (<CountryInformation country={selectedCountry} />) : (<CountryList countries={countries} onSelect={handleSelectCountry} />)}
     </div>
   )
 }
