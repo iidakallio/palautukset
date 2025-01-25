579ac0be908444dbc286bf765e0dcdbb3791e58c
ikallio <iida.kallio@aalto.fi>
2025-01-25 19:08:29 +0200

    eslint sääntöjen lisäys

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,70 +1,70 @@
 const _ = require('lodash')
 
-const dummy = (blogs) => {
-    return 1
+const dummy = () => {
+  return 1
 }
-  
+
 const totalLikes = (blogs) => {
-    return blogs.reduce((sum, blog) => sum + blog.likes, 0)
-  }
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
 
 const favoriteBlog = (blogs) => {
-    if (blogs.length === 0) {
-        return null 
-    }
+  if (blogs.length === 0) {
+    return null
+  }
 
-    const favorite = blogs.reduce((previous, current) => {
-        return current.likes > previous.likes ? current : previous
-    })
+  const favorite = blogs.reduce((previous, current) => {
+    return current.likes > previous.likes ? current : previous
+  })
 
-    return {
-        title: favorite.title,
-        author: favorite.author,
-        likes: favorite.likes,
-    }
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes,
+  }
 }
 
 const mostBlogs = (blogs) => {
-    if (blogs.length === 0) {
-        return null
-    }
+  if (blogs.length === 0) {
+    return null
+  }
 
-    const blogsAuthor = _.countBy(blogs, 'author')
-    console.log(blogsAuthor)
+  const blogsAuthor = _.countBy(blogs, 'author')
+  console.log(blogsAuthor)
 
-    const authorMostBlogs = _.maxBy( Object.entries(blogsAuthor), 
+  const authorMostBlogs = _.maxBy( Object.entries(blogsAuthor),
     ([, blogCount]) => blogCount)
-    return {
-        author: authorMostBlogs[0],
-        blogs: authorMostBlogs[1],
-    }
+  return {
+    author: authorMostBlogs[0],
+    blogs: authorMostBlogs[1],
+  }
 
 }
 
 const mostLikes = (blogs) => {
-    if (blogs.length === 0) {
-        return null
-    }
-    const blogsAuthor = _.groupBy(blogs, 'author')
-    console.log('blogs', blogsAuthor)
-    const authorLikes = _.map(blogsAuthor, (blogs, author) => ({
-        author,
-        likes: _.sumBy(blogs, 'likes'),
-    }))
-    console.log('likes',authorLikes)
-    const authorMostLikes = _.maxBy(authorLikes, 'likes')
-    console.log(authorMostLikes)
+  if (blogs.length === 0) {
+    return null
+  }
+  const blogsAuthor = _.groupBy(blogs, 'author')
+  console.log('blogs', blogsAuthor)
+  const authorLikes = _.map(blogsAuthor, (blogs, author) => ({
+    author,
+    likes: _.sumBy(blogs, 'likes'),
+  }))
+  console.log('likes',authorLikes)
+  const authorMostLikes = _.maxBy(authorLikes, 'likes')
+  console.log(authorMostLikes)
 
-    return {
-        author: authorMostLikes.author,
-        likes: authorMostLikes.likes,
-    }
+  return {
+    author: authorMostLikes.author,
+    likes: authorMostLikes.likes,
+  }
 }
-  
+
 module.exports = {
-    dummy,
-    totalLikes,
-    favoriteBlog,
-    mostBlogs,
-    mostLikes,
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes,
 }
