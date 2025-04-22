eed823f540f7f2c3155b0c8ad411c9e7af5108b9
ikallio <iida.kallio@aalto.fi>
2025-04-22 12:16:25 +0300

    blogien näkyvyys

--- osa4/bloglist-frontend/src/components/Blog.jsx
+++ osa4/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,34 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+import '../index.css'
+
+
+const Blog = ({ blog }) => {
+
+  const [visible, setVisible] = useState(false)
+
+  return (
+    <div className="blog">
+      {visible ? (
+        <div>
+          {blog.title} {blog.author}
+          <button onClick={() => setVisible(false)}>hide</button>
+          <div>{blog.url}</div>
+          <div>
+            likes {blog.likes}
+            <button>like</button>
+          </div>
+          <div>{blog.user?.name}</div>
+        </div>
+      ) : (
+        <div>
+          {blog.title} {blog.author}
+          <button onClick={() => setVisible(true)}>view</button>
+        </div>
+      )}
+    </div>
+  )
+}
+    
+
 
 export default Blog
