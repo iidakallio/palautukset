f880c0ccca19dc364af175b646edcff11bd70ecd
ikallio <iida.kallio@aalto.fi>
2025-07-03 15:44:00 +0300

    6.9. filteröinti reducer

--- osa6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ osa6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,7 +2,18 @@ import { useSelector, useDispatch } from 'react-redux'
 import { voteAnecdote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-    const anecdotes = useSelector(state => state)
+    const anecdotes = useSelector(state => {
+        const filter = state.filter
+        const anecdotes = state.anecdotes
+    
+        const filtered = filter === 'ALL'
+          ? anecdotes
+          : anecdotes.filter(anecdote =>
+              anecdote.content.toLowerCase().includes(filter.toLowerCase())
+            )
+    
+        return filtered
+      })
     const dispatch = useDispatch()
     
     const vote = (id) => {
